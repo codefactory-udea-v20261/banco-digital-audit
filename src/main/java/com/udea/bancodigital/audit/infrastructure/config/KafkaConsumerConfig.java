7797bc3d5c5831712a329e70ef79af6309777d00
Estefanía Garcés Pérez <47468501+egp1020@users.noreply.github.com>
2026-05-03 22:05:40 -0500

    fix: disable KafkaConsumerConfig in prod profile

--- src/main/java/com/udea/bancodigital/audit/infrastructure/config/KafkaConsumerConfig.java
+++ src/main/java/com/udea/bancodigital/audit/infrastructure/config/KafkaConsumerConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -20,6 +21,7 @@ import java.util.Map;
  * Kafka consumer configuration for Audit Service.
  */
 @Configuration
+@Profile("!prod")
 @EnableKafka
 public class KafkaConsumerConfig {
 
